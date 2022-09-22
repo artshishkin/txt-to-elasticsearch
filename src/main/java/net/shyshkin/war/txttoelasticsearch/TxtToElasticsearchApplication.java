ed4e5e742b0bacf1b3830f75610e6047269a4d94
Art <d.art.shishkin@gmail.com>
2022-09-22 15:48:09 +0300

    Initial commit

---
+++ src/main/java/net/shyshkin/war/txttoelasticsearch/TxtToElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.war.txttoelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxtToElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxtToElasticsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/shyshkin/war/txttoelasticsearch/TxtToElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.war.txttoelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxtToElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
