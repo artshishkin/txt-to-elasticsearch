47dc0f361300e547680925f2c5dcfb3da26c4bd0
Art <d.art.shishkin@gmail.com>
2022-09-23 12:24:10 +0300

    Make String Application Exit after Batch Job Finished

--- src/main/java/net/shyshkin/war/txttoelasticsearch/TxtToElasticsearchApplication.java
+++ src/main/java/net/shyshkin/war/txttoelasticsearch/TxtToElasticsearchApplication.java
@@ -2,12 +2,14 @@ package net.shyshkin.war.txttoelasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TxtToElasticsearchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TxtToElasticsearchApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(TxtToElasticsearchApplication.class, args);
+        System.exit(SpringApplication.exit(applicationContext));
     }
 
 }
