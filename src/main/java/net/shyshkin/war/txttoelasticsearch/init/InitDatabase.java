9a30e53ad34666ba603acf72d5ae5568899a64b6
Art <d.art.shishkin@gmail.com>
2022-09-26 12:00:17 +0300

    1.11 Init Database through ApplicationStartedEvent (#1)

--- src/main/java/net/shyshkin/war/txttoelasticsearch/init/InitDatabase.java
+++ src/main/java/net/shyshkin/war/txttoelasticsearch/init/InitDatabase.java
@@ -1,18 +1,22 @@
 package net.shyshkin.war.txttoelasticsearch.init;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
-//@Component
+@Slf4j
+@Component
 @RequiredArgsConstructor
 @Profile("xlsx-to-db")
-public class InitDatabase implements CommandLineRunner {
+public class InitDatabase {
 
     private final DataSource populationDataSource;
 
@@ -22,11 +26,13 @@ public class InitDatabase implements CommandLineRunner {
     @Value("jdbc/population-data.sql")
     private ClassPathResource dataDb;
 
-    @Override
-    public void run(String... args) throws Exception {
+    @EventListener(ApplicationStartedEvent.class)
+    public void applicationStarted() {
+        log.debug("Start initializing database");
         ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
         resourceDatabasePopulator.addScript(initDb);
         resourceDatabasePopulator.addScript(dataDb);
         resourceDatabasePopulator.execute(populationDataSource);
+        log.debug("Database initialization completed");
     }
 }
