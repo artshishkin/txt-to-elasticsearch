dfb38a913a219af40f9b6d6218d402bf513bd816
Art <d.art.shishkin@gmail.com>
2022-10-04 19:42:34 +0300

    6.5 City Repository with custom Query (#6)

--- src/main/java/net/shyshkin/war/txttoelasticsearch/repository/CityRepository.java
+++ src/main/java/net/shyshkin/war/txttoelasticsearch/repository/CityRepository.java
@@ -1,7 +1,15 @@
 package net.shyshkin.war.txttoelasticsearch.repository;
 
 import net.shyshkin.war.txttoelasticsearch.model.vk.City;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ReactiveElasticsearchRepository;
+import reactor.core.publisher.Mono;
 
 public interface CityRepository extends ReactiveElasticsearchRepository<City, Integer> {
+
+    Mono<City> findFirstByTitleOrAreaOrRegion(String title, String area, String region);
+
+    @Query("{\"bool\":{\"should\":[{\"match\":{\"title\":\"?0\"}},{\"match\":{\"area\":\"?0\"}},{\"match\":{\"region\":\"?0\"}}]}}")
+    Mono<City> findCityMatchingAddress(String address);
+
 }
