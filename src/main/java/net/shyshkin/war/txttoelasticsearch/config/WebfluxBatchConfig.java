ca5041bc4d16ffec982b1de858828aea140601ed
Art <d.art.shishkin@gmail.com>
2022-10-04 12:05:42 +0300

    4.7 Splitting flux of cities from web service through the buffer to fix issue (#4, #5)

--- src/main/java/net/shyshkin/war/txttoelasticsearch/config/WebfluxBatchConfig.java
+++ src/main/java/net/shyshkin/war/txttoelasticsearch/config/WebfluxBatchConfig.java
@@ -2,7 +2,6 @@ package net.shyshkin.war.txttoelasticsearch.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.shyshkin.war.txttoelasticsearch.model.City;
 import net.shyshkin.war.txttoelasticsearch.repository.CityRepository;
 import net.shyshkin.war.txttoelasticsearch.service.WebApiService;
 import org.springframework.batch.core.Job;
@@ -10,6 +9,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,7 +31,7 @@ public class WebfluxBatchConfig {
     @Bean
     Job readCityJob() {
         return jobs.get("Read Cities from web service and write it to Elasticsearch")
-//                .incrementer(new RunIdIncrementer())
+                .incrementer(new RunIdIncrementer())
                 .start(readCitiesFromWebServiceAndPopulateToElasticsearch())
                 .build();
     }
@@ -40,8 +40,9 @@ public class WebfluxBatchConfig {
     Step readCitiesFromWebServiceAndPopulateToElasticsearch() {
         return steps.get("Read Cities Data and Save to Elasticsearch")
                 .tasklet((contribution, chunkContext) -> {
-                    City last = cityRepository
-                            .saveAll(webApiService.getCities())
+                    var last = webApiService.getCities()
+                            .buffer(1000)
+                            .flatMap(cityRepository::saveAll)
                             .blockLast();
                     log.debug("Last city: {}", last);
                     return RepeatStatus.FINISHED;
