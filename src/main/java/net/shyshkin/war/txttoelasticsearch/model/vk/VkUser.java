e829dae96c9fbfbf2e9f9090465389d9e3d7d91c
Art <d.art.shishkin@gmail.com>
2022-10-07 14:53:30 +0300

    8.1 Populate many fields of VkUser (#8 More fields of Vk Account to save)

--- src/main/java/net/shyshkin/war/txttoelasticsearch/model/vk/VkUser.java
+++ src/main/java/net/shyshkin/war/txttoelasticsearch/model/vk/VkUser.java
@@ -1,6 +1,7 @@
 package net.shyshkin.war.txttoelasticsearch.model.vk;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
@@ -17,5 +18,37 @@ public class VkUser {
     private City city;
     private Country country;
     private LastSeen lastSeen;
+
+    private String deactivated;
+    @JsonProperty("is_closed")
+    private Boolean isClosed;
+    private String about;
+    private String activities;
+    private String books;
+    private JsonNode career;
+    private JsonNode connections;
+    private JsonNode contacts;
+    private JsonNode counters; //Поле возвращается только в методе users.get при запросе информации об одном пользователе, с передачей пользовательского access_token.
+    private String domain;
+    private JsonNode education;
+    private JsonNode exports;
+    private Integer followers_count;
+    private Integer has_mobile;
+    private Integer has_photo;
+    private String home_town;
+    private JsonNode military;
+    private String movies;
+    private String music;
+    private String nickname;
+    private JsonNode occupation;
+    private JsonNode personal;
+    private String quotes;
+    private JsonNode relatives;
+    private Integer relation;
+    private JsonNode schools;
+    private Integer sex;
+    private String site;
+    private String status;
+    private JsonNode universities;
 
 }
